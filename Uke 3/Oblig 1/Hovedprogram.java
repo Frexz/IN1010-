d76ed2dfe8978d4b1c59646c316a4edcddff942c
Frexz <fredrik.pettersen3@gmail.com>
2022-01-28 11:49:07 +0100

    Commit 28.01.2022

--- Uke 3/Oblig 1/Hovedprogram.java
+++ Uke 3/Oblig 1/Hovedprogram.java
@@ -3,23 +3,23 @@ import java.io.File;
 
 public class Hovedprogram {
     public static void main(String[] args) {
-        String filnavn = "dataklynge2.txt";
+        String filnavn = "dataklynge.txt";
 
         // Oppretter dataklynge
         Dataklynge saga = new Dataklynge();
 
         // Legger noder til dataklyngen fra fil
-        Hovedprogram.lesFraFil(filnavn);
+        Hovedprogram.lesFraFil(filnavn, saga);
 
         // Skriv ut informasjon om dataklyngen
-        /*System.out.printf("Noder med minst 128 GB: %d\n", saga.noderMedNokMinne(128));
+        System.out.printf("Noder med minst 128 GB: %d\n", saga.noderMedNokMinne(128));
         System.out.printf("Noder med minst 512 GB: %d\n", saga.noderMedNokMinne(512));
         System.out.printf("Noder med minst 1024 GB: %d\n\n", saga.noderMedNokMinne(1024));
         System.out.printf("Antall prosessorer: %d\n", saga.antProsessorer());
-        System.out.printf("Antall rack: %d\n", saga.antallRack());*/
+        System.out.printf("Antall rack: %d\n", saga.antallRack());
     }
 
-    public static void lesFraFil(String filnavn) {
+    public static void lesFraFil(String filnavn, Dataklynge klynge) {
     
         // Opretter et Scanner-objekt
         Scanner fil = null;
@@ -28,13 +28,26 @@ public class Hovedprogram {
             fil = new Scanner(new File(filnavn));
         } 
         catch (Exception e) {
-            System.out.println("Finner ikke filen du prøvde å lese.");        }
-        
-        // Leser innhold fra fil
-        while (fil.hasNextLine()) {
-            String[] info = fil.nextLine().split(" ");
-            
+            System.out.println("Finner ikke filen du prøvde å lese.");
         }
 
+        // Leser innhold fra fil og legger til node i dataklyngen
+        while (fil.hasNextLine()) {
+
+            // Leser informasjon fra fil
+            String[] info = fil.nextLine().split(" ");
+            int antallNoder = Integer.parseInt(info[0]);
+            int antallProsessorer = Integer.parseInt(info[1]);
+            int minne = Integer.parseInt(info[2]);
+
+            // Legger til noder i dataklynge
+            for (int i = 0; i < antallNoder; i++) {
+                klynge.settInnIRack(new Node(antallProsessorer, minne));
+            }
+
+        }
+
+        fil.close();
+        System.out.println("Noder fra fil har blitt lagt til i dataklynge.");
     }
 }
