ec7ef0a76f0ce6590ff55808108a9ceca6685ff6
Frexz <fredrik.pettersen3@gmail.com>
2022-02-09 23:45:15 +0100

    Commit 09.02.22

---
+++ Uke 5/Oblig 2/TestResepter.java
@@ -0,0 +1,13 @@
+public class TestResepter {
+    public static void main(String[] args) {
+        
+        // Oppretter nødvendige objekter
+        Vanlig vanlig = new Vanlig("Aerius", 85, 5);
+        Narkotisk narkotisk = new Narkotisk("Paralgin forte", 86, 60, 2);
+        Vanedannende vanedannende = new Vanedannende("Valium", 100, 5, 2);
+
+        Lege lege = new Lege();
+
+        
+    }
+}
