15ccfcd712a8311457b724c4b07e97bd8f6a1855
Frexz <fredrik.pettersen3@gmail.com>
2022-03-25 13:52:02 +0100

    Commit Oblig5 25.03.22

--- Uke 11/Oblig 5/SubsekvensRegister.java
+++ Uke 11/Oblig 5/SubsekvensRegister.java
@@ -2,24 +2,30 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Scanner;
 
-public class SubsekvensRegister {
+public class SubsekvensRegister implements Iterable<HashMap<String,Subsekvens>> {
     ArrayList<HashMap<String,Subsekvens>> register = new ArrayList<>();
 
+    // Legger til et HashMap i registeret
     public void settInn(HashMap<String,Subsekvens> subs) {
         register.add(subs);
     }
 
-    // TODO: Skal kunne ta ut et vilkårlig element, men usikker på hva som menes med det.
-    public HashMap<String,Subsekvens> hentHashMap(int index) {
-        return register.get(index);
+    // Henter ut et HashMap av registeret (det første)
+    public HashMap<String, Subsekvens> taUt() {
+        HashMap<String,Subsekvens> immurepetoar = register.get(0);
+        register.remove(0);
+        return immurepetoar;
     }
 
+    // Returnerer antall HashMap i registeret
     public int stoerrelse() {
         return register.size();
     }
 
+    // Leser og oppretter en HashMap med subsekvenser
     public static HashMap<String,Subsekvens> lesImmunrepetoar(String filnavn) {
         HashMap<String,Subsekvens> subs = new HashMap<>();
         Scanner fil = null;
@@ -32,7 +38,13 @@ public class SubsekvensRegister {
 
         // Leser hver linje
         while (fil.hasNextLine()) {
-            String sekvens = fil.next();
+            String sekvens = fil.nextLine();
+
+            // Avslutter programmet hvis en sekvens er kortere enn 3 lang
+            if (sekvens.length() < 3) {
+                System.out.println("En sekvens er kortere enn 3. Avslutter program ...");
+                System.exit(1);
+            }
             
             // Finner og lager mulige subsekvenser i linjen
             for (int i = 0; i < sekvens.length() - 2; i++) {
@@ -49,13 +61,16 @@ public class SubsekvensRegister {
         return subs;
     }
 
+    // Fletter to HashMaps med subsekvenser sammen til en HashMap med subsekvenser
     public static HashMap<String,Subsekvens> flettImmunrepetoar(HashMap<String,Subsekvens> hashmap1, HashMap<String,Subsekvens> hashmap2) {
         HashMap<String,Subsekvens> flettetSubs = new HashMap<>();
 
+        // Legger til subsekvensene i første HashMap
         for (String subsekvensString : hashmap1.keySet()) {
             flettetSubs.put(subsekvensString, hashmap1.get(subsekvensString));
         }
 
+        // Legger til subsekvensen i andre HashMap eller øker antallet hvis en subsekvens allerede eksisterer
         for (String subsekvensString : hashmap2.keySet()) {
             
             if (!flettetSubs.containsKey(subsekvensString)) {
@@ -66,5 +81,9 @@ public class SubsekvensRegister {
         }
 
         return flettetSubs;
+    }
+
+    public Iterator<HashMap<String,Subsekvens>> iterator() {
+        return register.iterator();
     }
 }
--- Uke 11/Oblig 5/TestSubsekvensRegister.java
+++ Uke 11/Oblig 5/TestSubsekvensRegister.java
@@ -1,14 +1,85 @@
-import java.util.Collection;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class TestSubsekvensRegister {
     public static void main(String[] args) {
-        HashMap<String,Subsekvens> hash1 = SubsekvensRegister.lesImmunrepetoar("Fil1.txt");
-        HashMap<String,Subsekvens> hash2 = SubsekvensRegister.lesImmunrepetoar("Fil2.txt");
+        ArrayList<String> paths = new ArrayList<>();
+        SubsekvensRegister subregTestDataLike = new SubsekvensRegister();
+        SubsekvensRegister subregTestDataLitenLike = new SubsekvensRegister();
 
-        for (Subsekvens sub : SubsekvensRegister.flettImmunrepetoar(hash1, hash2).values()) {
-            System.out.println(sub);
+        // Henter og sjekker om det finnes en metadata.csv fil i mappene som er argumenter til programmet og tar vare på absoluttstien dems i en liste.
+        for (int i = 0; i < args.length; i++) {
+                File f = new File(args[i] + "/metadata.csv");
+
+                if (!f.isFile()) {
+                    continue;
+                }
+
+                paths.add(f.getAbsolutePath());
         }
         
+        for (String path : paths) {
+            if (path.contains("TestDataLitenLike")) {
+                befolkSubsekvensRegister(path, subregTestDataLitenLike);
+            } else {
+                befolkSubsekvensRegister(path, subregTestDataLike);
+            }
+        }
+
+        // Fletter sammen alle HashMaps i registrene
+        flettAlle(subregTestDataLitenLike);
+        flettAlle(subregTestDataLike);
+
+        // Subsekvens med flest antall i TestDataLitenLike
+        System.out.println("TestDataLitenLike: " + finnFlest(subregTestDataLitenLike.taUt()));
+
+        // Subsekvens med flest antall i TestDataLike
+        System.out.println("TestDataLike: " + finnFlest(subregTestDataLike.taUt()));
+
+    }
+
+    // Leser metadata.csv filene, lager og legger til immunrepetoarer i registeret
+    public static void befolkSubsekvensRegister(String path, SubsekvensRegister subreg) {
+        Scanner metafil = null;
+        try {
+            metafil = new Scanner(new File(path));
+        } catch (FileNotFoundException e) {
+            System.out.println("Fant ikke filen " + path);
+            System.exit(1);
+        }
+
+        while (metafil.hasNextLine()) {
+            String[] data = metafil.nextLine().split(",");
+            String filnavn = path.replace("metadata.csv", data[0]);
+
+            subreg.settInn(SubsekvensRegister.lesImmunrepetoar(filnavn));
+        }
+    }
+
+    // Fletter alle HashMap i registeret og setter inn én HashMap (det er nå bare ett HashMap i registeret)
+    public static void flettAlle(SubsekvensRegister subreg) {
+        HashMap<String,Subsekvens> kombinertImmunrepetoar = new HashMap<>();
+        while (subreg.stoerrelse() != 0) {
+            kombinertImmunrepetoar = SubsekvensRegister.flettImmunrepetoar(kombinertImmunrepetoar, subreg.taUt());
+        }
+        subreg.settInn(kombinertImmunrepetoar);
+    }
+
+    // Finner subsekvens med flest forkomster i et immunrepetoar
+    public static Subsekvens finnFlest(HashMap<String, Subsekvens> immunrepetoar) {
+        int flestAntall = 0;
+        Subsekvens subsekvensMedFlestAntall = null;
+
+        for (Subsekvens subsekvens : immunrepetoar.values()) {
+            if (subsekvens.hentAntall() > flestAntall) {
+                flestAntall = subsekvens.hentAntall();
+                subsekvensMedFlestAntall = subsekvens;
+            }
+        }
+
+        return subsekvensMedFlestAntall;
     }
 }
